012b51745549a5dc4479a6b06a465da071773f35
Laura <laur730t@stud.kea.dk>
2022-08-24 12:16:42 +0200

    first commit

---
+++ java.js
@@ -0,0 +1,20 @@
+//Laver konstanter
+let overskrift = document.querySelector("h1");
+overskrift.textContent = "Billede";
+overskrift.innerHTML = "<h1>noget andet</h1>";
+
+//udskift overskrift h3
+const lilleo = document.querySelector("h3");
+lilleo.textContent = "Artikel overskrift";
+
+const tekst = document.querySelector("p");
+tekst.textContent = "tekst til artikler";
+console.log(tekst);
+
+// const underoverskrift = document.querySelector("h2");
+// console.log(underoverskrift);
+// const foto = document.querySelector("img");
+// console.log(foto);
+
+// const footer = document.querySelector("footer");
+// console.log(footer);

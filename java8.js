ebaad0668b2e4258fd0c2e88b4716cd8a128181b
Laura <laur730t@stud.kea.dk>
2022-08-24 15:45:35 +0200

    øvelse 8

---
+++ java8.js
@@ -0,0 +1,15 @@
+//Laver konstanter
+const playliste = [
+  "One time",
+  "Favorit Girl",
+  "Down to Earth",
+  "Bigger",
+  "One Less Lonely Girl",
+  "First Dance",
+  "Love Me",
+  "Common Denominator",
+  "Baby",
+  "Somebody To Love",
+];
+
+playliste.forEach((nummer) => (display.innerHTML += "<li>" + nummer + "</li>"));
